c0026f80ef4da3abb3db990646d049048baaeb91
DatTran <khiemtran21012000@gmail.com>
2023-05-09 10:47:13 +0700

    first commit

---
+++ src/main/java/com/spring/versiontwo/VersionTwoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.versiontwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersionTwoApplication.class, args);
+	}
+
+}
