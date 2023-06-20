0960dfc6e416c63c57f473e79549bf7474c019a0
DatTran <khiemtran21012000@gmail.com>
2023-06-20 21:57:15 +0700

    add code stureture

---
+++ src/main/java/com/spring/versiontwo/constant/StatusConstant.java
@@ -0,0 +1,8 @@
+package com.spring.versiontwo.constant;
+
+public final class StatusConstant {
+    public static final int STATUS_ACTIVE = 1;
+    public static final int STATUS_DELETED = 0;
+    public static final String ACTIVE = "ACTIVE";
+    public static final String DELETED = "DELETED";
+}
